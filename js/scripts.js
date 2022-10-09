b92c72597a6e2601c6e292fe8a00214f810e7300
jordanl412 <jordanlazan@gmail.com>
2022-10-09 14:20:21 -0400

    Added pokemonList array
    Added Bulbasaur, Jigglypuff, Seel, and Squirtle

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    {name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal']},
+    {name: 'Seel', height: 1.1, types: ['water']},
+    {name: 'Squirtle', height: 0.5, types: ['water']}
+];
