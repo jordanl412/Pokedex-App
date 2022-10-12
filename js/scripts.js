221bb0ad5bb02ce87772407923bce642fbe30eff
jordanl412 <jordanlazan@gmail.com>
2022-10-12 16:11:27 -0400

    Added IIFE
    Put pokemonList, and two functions (.add and .getAll) inside an IIFE, set to the variable pokemonRepository.
    
    Also used .add function to add Mr. Mime to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,36 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+    let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
     {name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal']},
     {name: 'Seel', height: 1.1, types: ['water']},
     {name: 'Squirtle', height: 0.5, types: ['water']}
-];
+    ];
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
+
 let editedPokemonList = '<ul class="pokemon-list">';
 
 document.write(editedPokemonList);
 
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.add({
+    name: 'Mr. Mime',
+    height: 1.3,
+    types: ['psychic', 'fairy']
+})
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     if (pokemon.height>1) {
         editedPokemonList += '<li>' + document.write(pokemon.name + 
         ' (height: ' + pokemon.height + ') - Wow, that\'s big!<br>' + '</li>');
@@ -18,7 +40,7 @@ pokemonList.forEach(function(pokemon) {
         ' (height: ' + pokemon.height + ')<br>' + '</li>');
     }
 })
-editedPokemonList += '</ul';
+editedPokemonList += '</ul>';
 
 
 
